cbc791cdcc782f2353558ac35b48b0b70a5b4351
T3rminator2297 <francis2297@hotmail.com>
2023-12-13 00:03:35 -0500

    Ajout d'un menu reactif

--- app/src/main/java/ca/qc/bdeb/a17/myapplication/MainActivity.kt
+++ app/src/main/java/ca/qc/bdeb/a17/myapplication/MainActivity.kt
@@ -2,6 +2,10 @@ package ca.qc.bdeb.a17.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +14,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Rendre la toolbar actif
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
     }
+
+    //Rendre le menu actif
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    //Définir les action de chaque item du menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId)
+        {
+            R.id.item1 ->
+            {
+                Toast.makeText(this, "Item 1 sélectionné", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.item2 ->
+            {
+                Toast.makeText(this, "Item 2 sélectionné", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.item3 ->
+            {
+                Toast.makeText(this, "Item 3 sélectionné", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
