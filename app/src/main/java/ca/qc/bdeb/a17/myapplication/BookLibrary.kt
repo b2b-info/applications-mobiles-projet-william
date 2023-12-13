5f5c8d6887d78d1d85f504d476ff44e38c5c9c19
T3rminator2297 <francis2297@hotmail.com>
2023-12-13 14:33:45 -0500

    Ajout de la Database et ajout de l'activité ajouterLivre

--- app/src/main/java/ca/qc/bdeb/a17/myapplication/BookLibrary.kt
+++ app/src/main/java/ca/qc/bdeb/a17/myapplication/BookLibrary.kt
@@ -1,20 +1,46 @@
 package ca.qc.bdeb.a17.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class BookLibrary : AppCompatActivity()
 {
+    lateinit var recyclerView: RecyclerView
+    lateinit var add_button: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_library)
 
+        //TOOLBAR
         // Récupérer la référence de la Toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
         // Changer le label de la Toolbar
         toolbar.title = "Bibliothèque"
+
+        //Menu
+        //Initialiser le menu
+        fun onCreateOptionsMenu(menu: Menu?): Boolean
+        {
+            menuInflater.inflate(R.menu.main_menu, menu)
+            return true
+        }
+        //DATABASE
+        //Ajout du recylclerView et du add button
+        recyclerView = findViewById(R.id.recyclerView);
+        add_button = findViewById(R.id.add_button)
+        add_button.setOnClickListener {
+            val intent = Intent(this@BookLibrary, AddActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
