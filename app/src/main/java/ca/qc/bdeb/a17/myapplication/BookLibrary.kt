7d91da86def9d3fe672448bb05041729674c1639
T3rminator2297 <francis2297@hotmail.com>
2023-12-13 14:53:09 -0500

    Correction de l'affichage du menu

--- app/src/main/java/ca/qc/bdeb/a17/myapplication/BookLibrary.kt
+++ app/src/main/java/ca/qc/bdeb/a17/myapplication/BookLibrary.kt
@@ -9,38 +9,37 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class BookLibrary : AppCompatActivity()
-{
+class BookLibrary : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var add_button: FloatingActionButton
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_library)
 
-        //TOOLBAR
+        // TOOLBAR
         // Récupérer la référence de la Toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         // Changer le label de la Toolbar
         toolbar.title = "Bibliothèque"
+        setSupportActionBar(toolbar)  // Ajouter cette ligne pour configurer la Toolbar comme ActionBar
 
-        //Menu
-        //Initialiser le menu
-        fun onCreateOptionsMenu(menu: Menu?): Boolean
-        {
-            menuInflater.inflate(R.menu.main_menu, menu)
-            return true
-        }
-        //DATABASE
-        //Ajout du recylclerView et du add button
-        recyclerView = findViewById(R.id.recyclerView);
+        // DATABASE
+        // Ajout du recycler view et du add button
+        recyclerView = findViewById(R.id.recyclerView)
         add_button = findViewById(R.id.add_button)
         add_button.setOnClickListener {
             val intent = Intent(this@BookLibrary, AddActivity::class.java)
             startActivity(intent)
-
         }
+    }
 
-
+    // MENU
+    // Initialiser le menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
+
+
