b7ec3e45395fccb9600d7adffda5a9a1f65e41f3
WilliamLebeuf <lebeufwilliam@gmail.com>
2023-12-13 21:49:52 -0500

    added recyclerview and database relation to list_activity layout

--- app/src/main/java/ca/qc/bdeb/a17/myapplication/ListActivity.kt
+++ app/src/main/java/ca/qc/bdeb/a17/myapplication/ListActivity.kt
@@ -3,17 +3,35 @@ package ca.qc.bdeb.a17.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.forEach
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ca.qc.bdeb.a17.myapplication.DAO.Entity.Book
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ListActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_activity)
-        // TOOLBAR
-        // Récupérer la référence de la Toolbar
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        // Changer le label de la Toolbar
         toolbar.title = "Liste des livres"
+        setSupportActionBar(toolbar)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView3)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val books = BookLibrary.database.bookDao().getAllBooks()
+
+            withContext(Dispatchers.Main) {
+                val adapter = BookAdapter(books)
+                recyclerView.adapter = adapter
+                recyclerView.layoutManager = LinearLayoutManager(this@ListActivity)
+            }
+        }
     }
 }
