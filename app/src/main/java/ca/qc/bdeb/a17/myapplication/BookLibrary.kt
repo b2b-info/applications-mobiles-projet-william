b554f2df8183d4179465befab81790adc6121ff2
WilliamLebeuf <lebeufwilliam@gmail.com>
2023-12-13 20:50:35 -0500

    function added, liste des livres is now selectable

--- app/src/main/java/ca/qc/bdeb/a17/myapplication/BookLibrary.kt
+++ app/src/main/java/ca/qc/bdeb/a17/myapplication/BookLibrary.kt
@@ -3,6 +3,7 @@ package ca.qc.bdeb.a17.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
@@ -46,6 +47,19 @@ class BookLibrary : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.list_activity -> openListActivity()
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openListActivity() {
+        val intent = Intent(this, ListActivity::class.java)
+        startActivity(intent)
     }
 }
 
--- app/src/main/java/ca/qc/bdeb/a17/myapplication/ListActivity.kt
+++ app/src/main/java/ca/qc/bdeb/a17/myapplication/ListActivity.kt
@@ -2,12 +2,18 @@ package ca.qc.bdeb.a17.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class ListActivity : AppCompatActivity() {
 
-  /*  override fun onCreate(savedInstanceState: Bundle?)
+    override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_activity)
-    }*/
+        // TOOLBAR
+        // Récupérer la référence de la Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        // Changer le label de la Toolbar
+        toolbar.title = "Liste des livres"
+    }
 }
