8dc5bc2aaba9c7a27ff73ae678834a6733bbd910
WilliamLebeuf <lebeufwilliam@gmail.com>
2023-12-13 19:41:31 -0500

    added ListActivity class

---
+++ app/src/main/java/ca/qc/bdeb/a17/myapplication/ListActivity.kt
@@ -0,0 +1,13 @@
+package ca.qc.bdeb.a17.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_activity)
+    }
+}
